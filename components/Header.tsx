9a3b22b58501cc2c0efe94f27be01fcfd75d26dd
Edmond Tam <edmondtam2@gmail.com>
2023-08-18 19:02:10 +0100

    Saving work - done steps 1, 2 and 3 of the quickstart

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import { Logo } from './Logo'
 
 const Header = () => {
   return (
@@ -15,13 +15,6 @@ const Header = () => {
             <div className="mr-3">
               <Logo />
             </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
           </div>
         </Link>
       </div>

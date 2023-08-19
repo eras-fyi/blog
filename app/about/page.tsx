2449173dd02aa8abd91c4a3ed35c423b859963e1
Edmond Tam <edmondtam2@gmail.com>
2023-08-19 23:26:10 +0100

    Add blogposts in

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,14 +7,23 @@ import { genPageMetadata } from 'app/seo'
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
-
   return (
     <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
+      <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 -mb-8">
+        About us
+      </h1>
+      {allAuthors.map((author) => {
+        if (author._id.includes('default')) return null
+        const mainContent = coreContent(author)
+        return (
+          <AuthorLayout key={author._id} content={mainContent}>
+            <div key={author.slug}>
+              <h1>{author.name}</h1>
+              <MDXLayoutRenderer code={author.body.code} />
+            </div>
+          </AuthorLayout>
+        )
+      })}
     </>
   )
 }
